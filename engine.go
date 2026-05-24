e59a93ab10f6edb2215255f99f5c8ca0a46281da
Elliot Wright <wright.elliot@gmail.com>
2026-05-24 12:17:16 +0100

    Tidied up a little bit of Engine

--- engine.go
+++ engine.go
@@ -2,13 +2,16 @@ package morph
 
 // Engine acts as the library entrypoint to Morph, exposing functionality found in Morph's
 // sub-packages from a convenient location.
-type Engine struct{}
+type Engine struct {
+	planner *Planner
+}
 
 func New() *Engine {
-	return &Engine{}
+	return &Engine{
+		planner: NewPlanner(),
+	}
 }
 
 func (e *Engine) Plan(spec Spec) (Plan, error) {
-	// TODO: No?
-	return NewPlanner().Plan(spec)
+	return e.planner.Plan(spec)
 }
